3db48cdd1157ab9dac44750d9cc7807a68c6eb46
unknown <shahrukh1494@gmail.com>
2021-05-12 12:59:26 +0530

    first commit

---
+++ pages/AnotherNewComponent.tsx
@@ -0,0 +1,3 @@
+export default function AnotherNewComponent() {
+  return <h1>This component is awesome too</h1>;
+}
---
+++ pages/NewComponent.tsx
@@ -0,0 +1,3 @@
+export default function NewComponent() {
+  return <h1>This component is awesome</h1>;
+}
